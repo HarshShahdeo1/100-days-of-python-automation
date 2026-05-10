0fc2e283a5a8137a61292d028d08af88b681bf3d
HarshShahdeo1 <harshshahdeo0@gmail.com>
2026-05-10 22:17:46 +0530

    refactor: comment out factorial and Fibonacci calculations in rough.py

--- basics/rough.py
+++ basics/rough.py
@@ -1,16 +1,16 @@
-n = int(input("Enter a number: "))
-fact = 1
-for i in range(1,n+1):
-    fact=fact*i
-print(f"Te factorisl {n} natural number is: {fact}")
-n=int(input("Enter a number: "))
-a=0
-b=1
-for i in range(0,n):
-    c=a+b
-    a=b
-    b=c
-print("The febonacci no are: ",a)
+# n = int(input("Enter a number: "))
+# fact = 1
+# for i in range(1,n+1):
+#     fact=fact*i
+# print(f"Te factorisl {n} natural number is: {fact}")
+# n=int(input("Enter a number: "))
+# a=0
+# b=1
+# for i in range(0,n):
+#     c=a+b
+#     a=b
+#     b=c
+# print("The febonacci no are: ",a)
 n = int(input("Enter a number: "))
 
 for i in range(1,n+1):
